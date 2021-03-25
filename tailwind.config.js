fd6674126d1d9c408ce4f3d1a0121092b4846118
brraiin <gskynn@gmail.com>
2021-03-25 16:35:04 +0100

    First

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	purge: {
+		mode: 'layers',
+		content: ['./public/**/*.html'],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+			},
+			colors: {
+				background: {
+					gray: '#121212',
+					lightgray: '#181818',
+					extralightgray: '#212121',
+					highlight: '#282828',
+					turquoise: '#4f746e',
+				},
+			},	  
+		},
+	},
+	variants: {
+		extend: {
+			padding: ['last'],
+			margin: ['group-hover', 'last'],
+			width: ['last'],
+			position: ['group-hover'],
+			ringColor: ['hover'],
+		},
+	},
+	plugins: [
+	],
+};
